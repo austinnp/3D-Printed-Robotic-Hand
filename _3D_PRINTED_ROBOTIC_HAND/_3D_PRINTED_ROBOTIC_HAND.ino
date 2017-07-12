41956cb23aa7856c451795f7b797dd92545f3289
austinnp <tinghao.peng@gmail.com>
2017-07-12 12:11:14 -0700

    FINAL CODE but still need to refine on comments so people understand the code

--- _3D_PRINTED_ROBOTIC_HAND/_3D_PRINTED_ROBOTIC_HAND.ino
+++ _3D_PRINTED_ROBOTIC_HAND/_3D_PRINTED_ROBOTIC_HAND.ino
@@ -32,7 +32,7 @@ void loop() {
   int flexpos; 
   int servopos;
   flexpos = analogRead(FLEX_PIN); //reads analog value from arduino
-  servopos = map(flexpos, 640, 430, 0, 180);  //maps analog values to 0 to 180 degrees of servo rotation
+  servopos = map(flexpos, 640, 430, 0, 90);  //maps analog values to 0 to 180 degrees of servo rotation
   servopos = constrain(servopos, 0, 180);     //constrains servo rotation to be between 0 and 180 degrees
   servo.write(servopos);    //tells servo to turn however many degrees accordingly to the analog values arduino takes in
   //the line of code below is for debugging purposes so you can find the values of your flex sensors
@@ -41,15 +41,15 @@ void loop() {
   int flexpos1;
   int servopos1;
   flexpos1 = analogRead(FLEX_PIN1);
-  servopos1 = map(flexpos1, 650, 440, 0, 180); 
+  servopos1 = map(flexpos1, 640, 410, 0, 115); 
   servopos1 = constrain(servopos1, 0, 180);
   servo1.write(servopos1);
-  //Serial.println("1: " + String(flexpos1));
+  Serial.println("1: " + String(flexpos1));
   
   int flexpos2;
   int servopos2;
   flexpos2 = analogRead(FLEX_PIN2);
-  servopos2 = map(flexpos2, 680, 450, 0, 180); 
+  servopos2 = map(flexpos2, 680, 450, 0, 110); 
   servopos2 = constrain(servopos2, 0, 180);
   servo2.write(servopos2);
   //Serial.println("2: " + String(flexpos2));
@@ -57,7 +57,7 @@ void loop() {
   int flexpos3;
   int servopos3;
   flexpos3 = analogRead(FLEX_PIN3);
-  servopos3 = map(flexpos3, 650, 450, 0, 180); 
+  servopos3 = map(flexpos3, 650, 450, 0, 105); 
   servopos3 = constrain(servopos3, 0, 180);
   servo3.write(servopos3);
   //Serial.println("3: " + String(flexpos3));
@@ -70,6 +70,6 @@ void loop() {
   servo4.write(servopos4);
   //Serial.println("4: " + String(flexpos4));
 
-  delay(200);
+  delay(50);
 }
 
