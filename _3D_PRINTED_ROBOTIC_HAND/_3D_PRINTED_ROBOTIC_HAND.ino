c95393a8919c7c262d7aa58958842e25117e311e
austinnp <tinghao.peng@gmail.com>
2017-07-20 10:52:56 -0700

    FINAL VERSION
    3D Printed Robotic Hand Final Code

--- _3D_PRINTED_ROBOTIC_HAND/_3D_PRINTED_ROBOTIC_HAND.ino
+++ _3D_PRINTED_ROBOTIC_HAND/_3D_PRINTED_ROBOTIC_HAND.ino
@@ -4,8 +4,9 @@ const int FLEX_PIN2 = A2;
 const int FLEX_PIN3 = A3;
 const int FLEX_PIN4 = A4;
 
+
 #include <Servo.h>
-Servo servo;        // creates servo object to control a servo
+Servo servo;  // creates servo object to control a servo
 Servo servo1; 
 Servo servo2;
 Servo servo3;
@@ -17,14 +18,15 @@ void setup() {
   servo.attach(11); //attaches servo to a digital pin on the arduino
   servo1.attach(9);
   servo2.attach(6);
-  servo3.attach(5);
-  servo4.attach(3);
+  servo3.attach(3);
+  servo4.attach(5);
 
   servo.write(0);   //sets the servo to 0
   servo1.write(0);
   servo2.write(0);
   servo3.write(0);
   servo4.write(0);
+
   delay(3000);
 }
 
@@ -32,11 +34,11 @@ void loop() {
   int flexpos; 
   int servopos;
   flexpos = analogRead(FLEX_PIN); //reads analog value from arduino
-  servopos = map(flexpos, 640, 430, 0, 90);  //maps analog values to 0 to 180 degrees of servo rotation
+  servopos = map(flexpos, 650, 340, 0, 90);  //maps analog values to 0 to 180 degrees of servo rotation
   servopos = constrain(servopos, 0, 180);     //constrains servo rotation to be between 0 and 180 degrees
   servo.write(servopos);    //tells servo to turn however many degrees accordingly to the analog values arduino takes in
   //the line of code below is for debugging purposes so you can find the values of your flex sensors
-  //Serial.println("0: " + String(flexpos));
+  Serial.println("0: " + String(flexpos));
   
   int flexpos1;
   int servopos1;
@@ -49,7 +51,7 @@ void loop() {
   int flexpos2;
   int servopos2;
   flexpos2 = analogRead(FLEX_PIN2);
-  servopos2 = map(flexpos2, 680, 450, 0, 110); 
+  servopos2 = map(flexpos2, 680, 450, 0, 140); //110
   servopos2 = constrain(servopos2, 0, 180);
   servo2.write(servopos2);
   //Serial.println("2: " + String(flexpos2));
@@ -57,10 +59,10 @@ void loop() {
   int flexpos3;
   int servopos3;
   flexpos3 = analogRead(FLEX_PIN3);
-  servopos3 = map(flexpos3, 650, 450, 0, 105); 
+  servopos3 = map(flexpos3, 670, 425, 0, 140); //105
   servopos3 = constrain(servopos3, 0, 180);
   servo3.write(servopos3);
-  //Serial.println("3: " + String(flexpos3));
+  Serial.println("3: " + String(flexpos3));
 
   int flexpos4;
   int servopos4;
@@ -70,6 +72,8 @@ void loop() {
   servo4.write(servopos4);
   //Serial.println("4: " + String(flexpos4));
 
-  delay(50);
+ 
+  delay(100);
 }
+
 
